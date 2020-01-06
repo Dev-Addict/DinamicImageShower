e22655684426b1bc199c4c8aa84ef08fc0509137
AriaMAN-ACT <aria.azadi.pour-act@protonmail.com>
2020-01-07 00:09:27 +0330

    security commit

---
+++ build/precache-manifest.65b5de8d3c3582012a602cdca2cb03d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72774425eaf2882884b663c72e3cb1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769069960f42bf6bed2a",
+    "url": "/static/js/2.7c0e3372.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.7c0e3372.chunk.js.LICENSE"
+  },
+  {
+    "revision": "188e3c764731b307316b",
+    "url": "/static/js/main.b32e4fd0.chunk.js"
+  },
+  {
+    "revision": "9d0dd3fdc844df518f35",
+    "url": "/static/js/runtime-main.8d1a7e68.js"
+  }
+]);
